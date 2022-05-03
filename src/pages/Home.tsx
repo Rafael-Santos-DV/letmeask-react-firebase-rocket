4c2630f87b767ce455de9b00dc69a693095c03d3
Rafael Santos <santosrafaell187@gmail.com>
2022-05-03 17:25:17 -0300

    add styles for page auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,25 @@ import illustrationimg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import "../styles/auth.scss";
+
 export function Home() {
   return(
-    <div>
-      <aside>
+    <div id="container">
+      <aside className="side-bar-illustration">
         <img src={illustrationimg} alt="Ilustração simbolizando perguntas e respostas." />
-        <strong>Crie salas de Q&clamp;A ao vivo</strong>
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvias da sua audiência em tempo real</p>
       </aside>
-      <main>
-        <div>
+      <main className="content-main">
+        <div className="content-main-login">
           <img src={logoImg} alt="LetMeask" />
           <button>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o google
           </button>
           <div>ou entre uma sala</div>
-          <form>
+          <form className="content-form">
             <input
               type="text"
               name=""
